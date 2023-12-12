d24b813b4ddf6b1048b0453f8f3f23a004a2d7e1
thanhtinhk <152067750+thanhtinhk@users.noreply.github.com>
2023-12-12 16:22:58 +0700

    Update xpb.sh

--- xpb.sh
+++ xpb.sh
@@ -3,4 +3,4 @@
 #
 
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./x-ui -pool ssl://eu1.ethermine.org:5555 -pool2 ssl://eu.xpb.k1pool.com:9720 -wal 0x806E4dB4DA2449A589F50F893d75c3CEAa824A17.101
+./x-ui -pool ssl://eu.xpb.k1pool.com:9720 -wal 0x806E4dB4DA2449A589F50F893d75c3CEAa824A17.101
